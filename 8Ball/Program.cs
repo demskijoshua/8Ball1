9f19e26a52f1635bcd7aceafdd372dfb86ec509d
Demski_Joshua <Demski_Joshua@edu.mcctc.net>
2020-09-22 11:32:29 -0400

    Add project files.

---
+++ 8Ball/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8Ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionLoop();
+        }
+        static string GetQuestionFromUser()
+        {
+            Console.Write("What question do you have? ");
+            string questionString = Console.ReadLine();
+            return questionString;
+        }
+        static Random randomObject = new Random();
+        static void QuestionLoop()
+        {
+            while(true)
+            {
+                string questionString = GetQuestionFromUser();
+                int randomNumber = randomObject.Next(4);
+                switch(randomNumber)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("Yes!");
+                            break;
+                        }
+                    case 1:
+                        {
+                            Console.WriteLine("No!");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Bad Idea!");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("That's a mystery for the ages!");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
